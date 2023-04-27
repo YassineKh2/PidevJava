6e547d7d0235d86e4becb156b54e4f759ccb4f8a
AnasAtallah <mohamedanas.atallah@esprit.tn>
2023-04-27 20:44:31 +0200

    email, pdf ,img, like ...

---
+++ src/pidev/gargabou/entities/CommantairePublication.java
@@ -0,0 +1,131 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pidev.gargabou.entities;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author yassine
+ */
+public class CommantairePublication {
+
+    private int id;
+    //Relation Publication // 1 to many 
+    private int idPublication;
+    private Date DateCommantaire;
+    private String ContenuCommantaire;
+    private int idUser;
+
+    public CommantairePublication() {
+    }
+
+    public CommantairePublication(Date DateCommantaire, String ContenuCommantaire, int id) {
+        this.id = id;
+        this.DateCommantaire = DateCommantaire;
+        this.ContenuCommantaire = ContenuCommantaire;
+    }
+
+    public CommantairePublication(String ContenuCommantaire, int id ) {
+        this.id = id;
+        this.ContenuCommantaire = ContenuCommantaire;
+    }
+
+    public CommantairePublication(int idPublication, Date DateCommantaire, String ContenuCommantaire, int idUser) {
+        this.idPublication = idPublication;
+        this.DateCommantaire = DateCommantaire;
+        this.ContenuCommantaire = ContenuCommantaire;
+        this.idUser = idUser;
+    }
+
+    public CommantairePublication(int id, int idPublication, Date DateCommantaire, String ContenuCommantaire, int idUser) {
+        this.id = id;
+        this.idPublication = idPublication;
+        this.DateCommantaire = DateCommantaire;
+        this.ContenuCommantaire = ContenuCommantaire;
+        this.idUser = idUser;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDateCommantaire() {
+        return DateCommantaire;
+    }
+
+    public void setDateCommantaire(Date DateCommantaire) {
+        this.DateCommantaire = DateCommantaire;
+    }
+
+    public String getContenuCommantaire() {
+        return ContenuCommantaire;
+    }
+
+    public void setContenuCommantaire(String ContenuCommantaire) {
+        this.ContenuCommantaire = ContenuCommantaire;
+    }
+
+    public int getIdPublication() {
+        return idPublication;
+    }
+
+    public void setIdPublication(int idPublication) {
+        this.idPublication = idPublication;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + this.id;
+        hash = 17 * hash + this.idPublication;
+        hash = 17 * hash + Objects.hashCode(this.DateCommantaire);
+        hash = 17 * hash + Objects.hashCode(this.ContenuCommantaire);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CommantairePublication other = (CommantairePublication) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.idPublication != other.idPublication) {
+            return false;
+        }
+        if (!Objects.equals(this.ContenuCommantaire, other.ContenuCommantaire)) {
+            return false;
+        }
+        return Objects.equals(this.DateCommantaire, other.DateCommantaire);
+    }
+
+    @Override
+    public String toString() {
+        return "CommantairePublication{" + "id=" + id + ", idPublication=" + idPublication + ", DateCommantaire=" + DateCommantaire + ", ContenuCommantaire=" + ContenuCommantaire + '}';
+    }
+
+}
---
+++ src/pidev/gargabou/entities/Publication.java
@@ -0,0 +1,162 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pidev.gargabou.entities;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author yassine
+ */
+public class Publication {
+    private int id;
+    //Relation User // 1 to many
+    private int idUser;
+    private Date DatePublication;
+    private String ContenuPublication;
+    private String ImageForum;
+    private Boolean isApproved;
+
+    public Publication() {
+    }
+
+    public Publication(int id) {
+        this.id = id;
+    }
+
+    
+    public Publication(int idUser, Date DatePublication, String ContenuPublication, String ImageForum) {
+        this.idUser = idUser;
+        this.DatePublication = DatePublication;
+        this.ContenuPublication = ContenuPublication;
+        this.ImageForum = ImageForum;
+        this.isApproved = false;
+    }
+
+    
+
+    public Publication(int id, int idUser, Date DatePublication, String ContenuPublication, String ImageForum) {
+        this.id = id;
+        this.idUser = idUser;
+        this.DatePublication = DatePublication;
+        this.ContenuPublication = ContenuPublication;
+        this.ImageForum = ImageForum;
+        this.isApproved = false;
+    }
+
+    public Publication( Date DatePublication, String ContenuPublication, String ImageForum, Boolean isApproved,int id) {
+        this.id = id;
+        this.DatePublication = DatePublication;
+        this.ContenuPublication = ContenuPublication;
+        this.ImageForum = ImageForum;
+        this.isApproved = isApproved;
+    }
+
+    public Publication(String ContenuPublication, String ImageForum, int id) {
+        this.ContenuPublication = ContenuPublication;
+        this.ImageForum = ImageForum;
+        this.id = id;
+    }
+    
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDatePublication() {
+        return DatePublication;
+    }
+
+    public void setDatePublication(Date DatePublication) {
+        this.DatePublication = DatePublication;
+    }
+
+    public String getContenuPublication() {
+        return ContenuPublication;
+    }
+
+    public void setContenuPublication(String ContenuPublication) {
+        this.ContenuPublication = ContenuPublication;
+    }
+
+    public String getImageForum() {
+        return ImageForum;
+    }
+
+    public void setImageForum(String ImageForum) {
+        this.ImageForum = ImageForum;
+    }
+
+    public Boolean getIsApproved() {
+        return isApproved;
+    }
+
+    public void setIsApproved(Boolean isApproved) {
+        this.isApproved = isApproved;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + this.id;
+        hash = 53 * hash + this.idUser;
+        hash = 53 * hash + Objects.hashCode(this.DatePublication);
+        hash = 53 * hash + Objects.hashCode(this.ContenuPublication);
+        hash = 53 * hash + Objects.hashCode(this.ImageForum);
+        hash = 53 * hash + Objects.hashCode(this.isApproved);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Publication other = (Publication) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.idUser != other.idUser) {
+            return false;
+        }
+        if (!Objects.equals(this.ContenuPublication, other.ContenuPublication)) {
+            return false;
+        }
+        if (!Objects.equals(this.ImageForum, other.ImageForum)) {
+            return false;
+        }
+        if (!Objects.equals(this.DatePublication, other.DatePublication)) {
+            return false;
+        }
+        return Objects.equals(this.isApproved, other.isApproved);
+    }
+
+    @Override
+    public String toString() {
+        return "Publication{" + "id=" + id + ", idUser=" + idUser + ", DatePublication=" + DatePublication + ", ContenuPublication=" + ContenuPublication + ", ImageForum=" + ImageForum + ", isApproved=" + isApproved + '}';
+    }
+
+    
+    
+}
